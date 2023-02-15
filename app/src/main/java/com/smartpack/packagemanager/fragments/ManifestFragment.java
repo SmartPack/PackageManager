SmartPack/PackageManager

commit 53277f0096c9cddcc572e02250fda2d322774563

Package Details: Make sure AndroidManifest.xml loaded
Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

app/src/main/java/com/smartpack/packagemanager/fragments/ManifestFragment.java
@@ -18,23 +18,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.apk.axml.APKParser;
+import com.apk.axml.aXMLDecoder;
 import com.smartpack.packagemanager.R;
 import com.smartpack.packagemanager.adapters.ManifestAdapter;
 import com.smartpack.packagemanager.utils.Common;
-import com.smartpack.packagemanager.utils.PackageExplorer;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.zip.ZipFile;
+
+import in.sunilpaulmathew.sCommon.Utils.sExecutor;
+import in.sunilpaulmathew.sCommon.Utils.sPackageUtils;
+import in.sunilpaulmathew.sCommon.Utils.sUtils;
 
 /*
  * Created by sunilpaulmathew <sunil.kde@gmail.com> on March 08, 2021
  */
 public class ManifestFragment extends Fragment {
-
-    private static final APKParser mAPKParser = new APKParser();
 
     @Nullable
     @Override
@@ -43,14 +46,34 @@ public class ManifestFragment extends Fragment {
 
         RecyclerView mRecyclerView = mRootView.findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
-        mRecyclerView.setAdapter(new ManifestAdapter(getData()));
+
+        new sExecutor() {
+            private List<String> mManifest = null;
+
+            @Override
+            public void onPreExecute() {
+            }
+
+            @Override
+            public void doInBackground() {
+                try (ZipFile zipFile = new ZipFile(sPackageUtils.getSourceDir(Common.getApplicationID(), requireActivity()))) {
+                    InputStream inputStream = zipFile.getInputStream(zipFile.getEntry("AndroidManifest.xml"));
+                    mManifest =  new ArrayList<>(Arrays.asList(Objects.requireNonNull(new aXMLDecoder().decode(inputStream).trim()).split("\\r?\\n")));
+                } catch (Exception ignored) {
+                }
+            }
+
+            @Override
+            public void onPostExecute() {
+                if (mManifest != null && mManifest.size() > 0) {
+                    mRecyclerView.setAdapter(new ManifestAdapter(mManifest));
+                } else {
+                    sUtils.snackBar(mRootView, getString(R.string.failed_decode_xml, "AndroidManifest.xml")).show();
+                }
+            }
+        }.execute();
 
         return mRootView;
-    }
-
-    private List<String> getData() {
-        return new ArrayList<>(Arrays.asList(Objects.requireNonNull(mAPKParser.getManifest() != null
-                ? mAPKParser.getManifest() : PackageExplorer.readManifest(Common.getSourceDir())).split("\\r?\\n")));
     }
 
 }

app/src/main/java/com/smartpack/packagemanager/activities/PackageDetailsActivity.java
@@ -16,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.viewpager.widget.ViewPager;
 
-import com.apk.axml.APKParser;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.textview.MaterialTextView;
 import com.smartpack.packagemanager.R;
@@ -74,9 +73,8 @@ public class PackageDetailsActivity extends AppCompatActivity {
         if (PackageDetails.getActivities(Common.getApplicationID(), this).size() > 0) {
             adapter.AddFragment(new ActivitiesFragment(), getString(R.string.activities));
         }
-        if (new APKParser().getManifest() != null) {
-            adapter.AddFragment(new ManifestFragment(), getString(R.string.manifest));
-        }
+        adapter.AddFragment(new ManifestFragment(), getString(R.string.manifest));
+
 
         mViewPager.setAdapter(adapter);
         mTabLayout.setupWithViewPager(mViewPager);

commit 8e088dfa255e57e99e92b67c96faf3bba42ed610

Manifest: Fixed AndroidManifest failed to load on APK picking This issue was accidently introduced with commit: 53277f0096c9cddcc572e02250fda2d322774563
Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

app/src/main/java/com/smartpack/packagemanager/fragments/ManifestFragment.java
@@ -18,6 +18,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.apk.axml.APKParser;
 import com.apk.axml.aXMLDecoder;
 import com.smartpack.packagemanager.R;
 import com.smartpack.packagemanager.adapters.ManifestAdapter;
@@ -56,10 +57,14 @@ public class ManifestFragment extends Fragment {
 
             @Override
             public void doInBackground() {
-                try (ZipFile zipFile = new ZipFile(sPackageUtils.getSourceDir(Common.getApplicationID(), requireActivity()))) {
-                    InputStream inputStream = zipFile.getInputStream(zipFile.getEntry("AndroidManifest.xml"));
-                    mManifest =  new ArrayList<>(Arrays.asList(Objects.requireNonNull(new aXMLDecoder().decode(inputStream).trim()).split("\\r?\\n")));
-                } catch (Exception ignored) {
+                if (Common.isAPKPicker()) {
+                    mManifest = new ArrayList<>(Arrays.asList(new APKParser().getManifest().trim().split("\\r?\\n")));
+                } else {
+                    try (ZipFile zipFile = new ZipFile(sPackageUtils.getSourceDir(Common.getApplicationID(), requireActivity()))) {
+                        InputStream inputStream = zipFile.getInputStream(zipFile.getEntry("AndroidManifest.xml"));
+                        mManifest = new ArrayList<>(Arrays.asList(Objects.requireNonNull(new aXMLDecoder().decode(inputStream).trim()).split("\\r?\\n")));
+                    } catch (Exception ignored) {
+                    }
                 }
             }
 