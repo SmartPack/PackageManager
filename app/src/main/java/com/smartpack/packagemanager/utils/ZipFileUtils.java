969b5d7cddd53eb9eb8dded7acfeca3573da198e
sunilpaulmathew <sunil.kde@gmail.com>
2023-02-19 17:55:06 +0100

    Simplified ZipFileUtils
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- app/src/main/java/com/smartpack/packagemanager/utils/ZipFileUtils.java
+++ app/src/main/java/com/smartpack/packagemanager/utils/ZipFileUtils.java
@@ -11,9 +11,9 @@ package com.smartpack.packagemanager.utils;
 import android.widget.ProgressBar;
 
 import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 /*
@@ -25,34 +25,26 @@ public class ZipFileUtils extends ZipFile {
         super(zipFile);
     }
 
-    public void unzip(String path) {
-        try (ZipFile zipFile = new ZipFile(toString())) {
-            zipFile.extractAll(path);
-        } catch (IOException ignored) {}
+    public void unzip(String path) throws ZipException {
+        extractAll(path);
     }
 
-    public void zip(List<File> files) {
-        try (ZipFile zipFile = new ZipFile(toString())) {
-            zipFile.addFiles(files);
-        } catch (IOException ignored) {
-        }
+    public void zip(List<File> files) throws ZipException {
+        addFiles(files);
     }
 
-    public void zip(List<File> files, ProgressBar progressBar) {
+    public void zip(List<File> files, ProgressBar progressBar) throws ZipException {
         if (progressBar != null) {
             progressBar.setMax(files.size());
         }
         for (File file : files) {
-            try (ZipFile zipFile = new ZipFile(toString())) {
-                zipFile.addFile(file);
-                if (progressBar != null) {
-                    if (progressBar.getProgress() < files.size()) {
-                        progressBar.setProgress(progressBar.getProgress() + 1);
-                    } else {
-                        progressBar.setProgress(0);
-                    }
+            addFile(file);
+            if (progressBar != null) {
+                if (progressBar.getProgress() < files.size()) {
+                    progressBar.setProgress(progressBar.getProgress() + 1);
+                } else {
+                    progressBar.setProgress(0);
                 }
-            } catch (IOException ignored) {
             }
         }
     }
